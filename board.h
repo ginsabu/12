bb9a3bbc2919daa369459e00f1df1943ce4f165c
ginsabu <155948685+ginsabu@users.noreply.github.com>
2025-12-21 22:16:47 +0900

    board.h

--- board.h
+++ board.h
@@ -1,13 +1,10 @@
+#define N_BOARD               20
+#define BOARDSTATUS_OK         1
+#define BOARDSTATUS_NOK        0
 
-
-int board_initBoard(void);
-int board_printBoardStatus(int pos);
+void board_printBoardStatus(void);
+int board_getBoardStatus(int pos);
 int board_getBoardCoin(int pos);
+void board_initBoard(void);
 int board_getSharkPosition(void);
 int board_stepShark(void);
-int board_getBoardStatus(int pos);
-int 
-
-
-
-
